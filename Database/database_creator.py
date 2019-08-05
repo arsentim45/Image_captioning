b135a52ad8116439aab3ad64bcbdd20b0f229305
arsentymchyshyn <arsen.tymchyshyn@logivations.com>
2019-08-05 15:24:52 +0300

    First version of database creation

---
+++ Database/database_creator.py
@@ -0,0 +1,11 @@
+import sqlite3
+def main():
+    conn = sqlite3.connect("main_database.db")
+    cursor = conn.cursor()
+    try:
+        cursor.execute("""CREATE TABLE pictures (link, description)""")
+        conn.commit()
+    except sqlite3.OperationalError:
+        print('Database already exist')
+if __name__ == '__main__':
+    main()
